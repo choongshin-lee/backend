a29a53283cbc3d1892b428e2fc54e266880c8e39
choongshin-lee <ramas@naver.com>
2023-08-30 18:05:21 +0900

    신규추가(수입검사불합격재발행/발주상세현황), 발주현황 수정

---
+++ src/main/resources/static/mm/XUMM2080.js
@@ -0,0 +1,21 @@
+var menuId = 'XUMM2080';
+var widget = momWidget;
+var that = undefined;
+var VIEW= {
+	initParam		: undefined, 
+	partnerCd       : undefined,
+	init: function() {	
+		that = this;	
+		that.event();
+	},
+	event: function(e) {
+	
+	},
+};
+
+$(document).ready(function(event){	
+	momSetup.init();
+	//momWidget.init(1, menuId, VIEW,'GRID');	
+	momWidget.init(1, menuId, VIEW);	
+	VIEW.init();
+});
