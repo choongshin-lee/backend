afe6c208a47b10ed8e8cb8792198b5b633394613
choongshin-lee <ramas@naver.com>
2023-08-30 11:43:31 +0900

    신규화면 추가(납품서/입고서현황)

---
+++ src/main/resources/static/mm/XUMM4040.js
@@ -0,0 +1,21 @@
+var menuId = 'XUMM4040';
+var widget = momWidget;
+var that = undefined;
+var VIEW= {
+	initParam		: undefined, 
+	partnerCd       : undefined,
+	init: function() {	
+		that = this;	
+		that.event();
+	},
+	event: function(e) {
+	
+	},
+};
+
+$(document).ready(function(event){	
+	momSetup.init();
+	//momWidget.init(1, menuId, VIEW,'GRID');	
+	momWidget.init(1, menuId, VIEW);	
+	VIEW.init();
+});
