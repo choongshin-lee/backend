db212faad41b8890c8289013d88f6db70b80ad87
choongshin-lee <ramas@naver.com>
2023-08-31 17:51:36 +0900

    구매요청반려 추가/수입검사 불합력 재발행 추가

--- src/main/resources/static/mm/XUMM3050.js
+++ src/main/resources/static/mm/XUMM3050.js
@@ -1,4 +1,4 @@
-var menuId = 'XUMM2070';
+var menuId = 'XUMM3050';
 var widget = momWidget;
 var that = undefined;
 var VIEW= {
@@ -10,16 +10,27 @@ var VIEW= {
 	},
 	event: function(e) {
 	    dateSelect();	
-	    $('#partnerCdSP1').jqxComboBox({disabled: true});
+	    $('#partnerCdSP1').jqxComboBox({disabled: true});	
 	},
+    searchCallInit: function(index,your,action,btnId,param,result,event) { //조회액션 실행 전에 호출되는 함수 
+        if(index==0 ){	                                             
+		    AUIGrid.clearGridData(widget.grid[1]);
+		}  
+	},
+	cellClickCallBack: function(index,rowIndex,target,e) {				
+		if(index==0){
+			widget.findBtnClicked(1, {departureNo:e.item['departureNo']}, true, 'CELLCLICK',menuId,VIEW);
+		}
+	}
 };
 
 $(document).ready(function(event){	
 	momSetup.init();
-	//momWidget.init(1, menuId, VIEW,'GRID');	
 	momWidget.init(1, menuId, VIEW);	
+	momWidget.init(2, menuId, VIEW);
 	VIEW.init();
 });
+
 
 function dateSelect(){
    mom_ajax('R', 'XUMM3010.dropdownlistContentpartnerCdSP1-select1', {}, function(result, data) {
--- src/main/resources/static/mm/XUMM4050.js
+++ src/main/resources/static/mm/XUMM4050.js
@@ -9,80 +9,24 @@ var VIEW= {
 		that.event();
 	},
 	event: function(e) {
-	
+	    dateSelect();	
+	    $('#partnerCdSP1').jqxComboBox({disabled: true});	
 	},
     searchCallInit: function(index,your,action,btnId,param,result,event) { //조회액션 실행 전에 호출되는 함수 
-        let checkItem = widget.getCheckedRowItems(widget.grid[0]);
         if(index==0 ){	                                             
 		    AUIGrid.clearGridData(widget.grid[1]);
-		}
-		else if(index==1){        
-            result.param = {processCd:'RD'};  
-        }  
-        else if(index==20){        
-            result.param = {processCd:'RD'};  
-        }    
+		}  
 	},
-	copyCallBack: function(index,your,action,btnId,param,result,data) {  //등록버튼 팝업띄우고나서 호출되는 함수 
-	    if(index ==0 && btnId =='copyBtn'){
-		    $('#receiptNoDP1').val('');		
-		}
-	},
-    cellClickCallBack: function(index,rowIndex,target,e) {				
+	cellClickCallBack: function(index,rowIndex,target,e) {				
 		if(index==0){
-			widget.findBtnClicked(1, {receiptNo:e.item['receiptNo']}, true, 'CELLCLICK',menuId,VIEW);
+			widget.findBtnClicked(1, {receiptNo['receiptNo']}, true, 'CELLCLICK',menuId,VIEW);
 		}
-	},
-	customCallInit: function(index,your,action,btnId,param,result) {
-	    var checkItem = widget.getCheckedRowItems(widget.grid[0]);
-		if(index == 1){
-		    if(btnId == 'customGridPopBtn2-1'){ // 커스텀 버튼 실행시 1 삭제(D) 2 TMP삽입(C) 3 프로시저실행(P) actionType 으로 시점 제어가능  		
-			    if(checkItem.length==0){
-				    result.msg = '상단에서 입고서 선택필수!';
-				    result.result = 'WARN';
-				    return;
-			    }
-	            $('#partnerCdSP21').val(checkItem[0]['partnerCd']);
-			    $('#receiptWarehouseCdSP21').val(checkItem[0]['receiptWarehouseCd']);
-			    $('#iqcFlagSP21').val(checkItem[0]['iqcFlag']);
-			    //$('#processCdSP21').val(checkItem[0]['processCd']);
-			    $('#partnerCdSP21').jqxComboBox({disabled: true});
-			    $('#receiptWarehouseCdSP21').jqxComboBox({disabled: true});
-			    $('#iqcFlagSP21').jqxComboBox({disabled: true});
-			    //$('#processCdSP21').jqxComboBox({disabled: true});
-			    $('#currencyCdSP21').val(checkItem[0]['currencyCd']);
-		        $('#currencyCdSP21').jqxComboBox({disabled: true});
-			}
-			else if(btnId == 'customBtn2-2' ){
-			    for(var i=0,max=param.length; i<max;i++){
-				    param[i]['currencyCd'] = checkItem[0]['currencyCd'];
-				    param[i]['exchangeRate'] = checkItem[0]['exchangeRate'];
-			    }
-		    }
-			else if(btnId == 'customBtn2-3'){
-			    for(var i=0,max=param.length; i<max;i++){
-				    param[i]['currencyCd'] = checkItem[0]['currencyCd'];
-				    param[i]['exchangeRate'] = checkItem[0]['exchangeRate'];
-			    }
-		    }		    
-		}
-	    else if(index == 20){			    
-		    if(action='C'&& btnId == 'customBtn21-1'){ 
-			    for(var i=0,max=param.length; i<max;i++){
-				    param[i]['inoutNo'] = checkItem[0]['receiptNo'];
-			    }
-		    }
-	    }
 	}
 };
 
 $(document).ready(function(event){	
 	momSetup.init();
-	//momWidget.init(1, menuId, VIEW,'GRID');	
-	//momWidget.init(2, menuId, VIEW,'GRID');	
 	momWidget.init(1, menuId, VIEW);	
 	momWidget.init(2, menuId, VIEW);
-	//momWidget.gridPopup.init(2,21,1,'XUSM8100', VIEW);
-	momWidget.gridPopup.init(2,21,1,'XUDG0420', VIEW);
 	VIEW.init();
 });
