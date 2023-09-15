31ae2d652a8574c0b9a93f2f489f02c42fd0b124
choongshin-lee <ramas@naver.com>
2023-09-15 17:16:56 +0900

    수요계획 선행 추가

--- src/main/resources/static/pp/XUPP3125.js
+++ src/main/resources/static/pp/XUPP3125.js
@@ -75,5 +75,6 @@ $(document).ready(function(event){
 	momSetup.init();
 	momWidget.init(1, menuId, VIEW);	
 	momWidget.init(2, menuId, VIEW);	
+	momWidget.gridPopup.init(2,21,1,'XUDG0500', VIEW);
 	VIEW.init();
 });
