fda806f1a6bb0b37d8ca62ca88d6beba17950126
choongshin-lee <ramas@naver.com>
2023-10-14 10:04:02 +0900

    납품서확정취소/현황-협력사용 분리 신규 추가

---
+++ src/main/resources/static/mm/XUMM3045.js
@@ -0,0 +1,22 @@
+var menuId = 'XUMM3045';
+var widget = momWidget;
+var that = undefined;
+var VIEW= {
+	initParam		: undefined, 
+	partnerCd       : undefined,
+	init: function() {	
+		that = this;	
+		that.event();
+	},
+	event: function(e) {
+	    	
+	},
+};
+
+$(document).ready(function(event){	
+	momSetup.init();
+	//momWidget.init(1, menuId, VIEW,'GRID');	
+	momWidget.init(1, menuId, VIEW);	
+	VIEW.init();
+});
+
